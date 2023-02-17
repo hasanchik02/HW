4665bd2c5a0c3a5f302c5eaa131a1fd65c0b0cb3
Slava <Slava9162004@yandex.ru>
2023-02-18 00:58:21 +0300

    FillArray

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿string [] Array (int size)
+{
+    string [] array = new string[size];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        string s = Console.ReadLine();
+        array[i] = s;    
+    }
+    return array;
+}
